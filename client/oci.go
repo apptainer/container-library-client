628b3bd1e8ac3cda1c8a39d50089d372f857e7d1
Mike Frisch <mikef17@gmail.com>
2023-02-10 14:29:06 -0500

    chore: add func comment
    Signed-off-by: Edita Kizinevic <edita.kizinevic@cern.ch>

--- client/oci.go
+++ client/oci.go
@@ -625,6 +625,7 @@ func (r *ociRegistry) getImageConfig(ctx context.Context, creds credentials, nam
 
 var errOCIDownloadNotSupported = errors.New("not supported")
 
+// newOCIRegistry returns *ociRegistry, credentials for that registry, and the (optionally) remapped image name
 func (c *Client) newOCIRegistry(ctx context.Context, name string, accessTypes []accessType) (*ociRegistry, *bearerTokenCredentials, error) {
 	// Attempt to obtain (direct) OCI registry auth token
 	registryURI, creds, err := c.ociRegistryAuth(ctx, name, accessTypes)
